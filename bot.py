11b9c72981e21da4ee1b64796e41fd92669ab1bc
kairo09x <kairo09x@gmail.com>
2025-12-28 03:26:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,12 @@
-from pyrogram import Client, enums
-from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID, LOGGER_ID
-import logging
-
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger(__name__)
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
 
 
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            name="MyMusicBot",
+            "AudioMusicBot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            parse_mode=enums.ParseMode.HTML,
         )
-
-        self.owner_id = OWNER_ID
-        self.logger_id = LOGGER_ID
-
-    async def start(self):
-        await super().start()
-        me = await self.get_me()
-        log.info(f"Started as @{me.username}")
-
-        if self.logger_id:
-            try:
-                await self.send_message(self.logger_id, "🎵 Music Bot Started")
-            except Exception as e:
-                log.warning(f"Logger group error: {e}")
-
-    async def stop(self):
-        await super().stop()
-        log.info("Bot stopped")
