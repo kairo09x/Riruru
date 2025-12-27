061e6d93687de526675006ba8b91f54a1919804e
kairo09x <kairo09x@gmail.com>
2025-12-28 03:25:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 OWNER_ID = int(os.getenv("OWNER_ID", 0))
-LOGGER_ID = int(os.getenv("LOGGER_ID", 0))
-
-DEFAULT_THUMB = os.getenv(
-    "DEFAULT_THUMB",
-    "https://telegra.ph/file/8b3f6c7e6a9a6b0b5c.jpg"
-)
-
-SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/")
