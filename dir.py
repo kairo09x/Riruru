a6110ed7025e0b71235f3f4f4b8ce2e2bdaa4ae7
kairo09x <kairo09x@gmail.com>
2025-12-28 19:24:43 +0530

    Delete dir.py

--- dir.py
+++
@@ -1,9 +0,0 @@
-import shutil
-from pathlib import Path
-
-
-def ensure_dirs():
-    if not shutil.which("ffmpeg"):
-        raise RuntimeError("FFmpeg not installed!")
-
-    Path("downloads").mkdir(exist_ok=True)
