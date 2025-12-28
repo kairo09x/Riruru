00518fc532450220f6dd87d82012518c1873d29a
kairo09x <kairo09x@gmail.com>
2025-12-28 11:49:28 +0530

    Update calls.py

--- calls.py
+++ calls.py
@@ -1,7 +1,7 @@
 from pytgcalls import PyTgCalls
 from pytgcalls.types import MediaStream, AudioQuality
-from queue import get_next, clear
 from utils import download_audio
+from queue import clear
 
 
 class CallManager:
@@ -19,14 +19,7 @@ class CallManager:
         )
         await self.call.play(chat_id, stream)
 
-    async def skip(self, chat_id):
-        next_track = get_next(chat_id)
-        if not next_track:
-            await self.call.leave_call(chat_id)
-            return False
-        await self.play(chat_id, next_track)
-        return True
-
     async def stop(self, chat_id):
         clear(chat_id)
         await self.call.leave_call(chat_id)
+)
